fef5158908e657473968346af82cf8cf66f3f8a3
Yadzan <104123429+YazdanMne@users.noreply.github.com>
2022-06-03 00:13:04 -0700

    University_project_fal_hafez

--- app/src/main/java/com/example/mobinaabasi/FallActivity.kt
+++ app/src/main/java/com/example/mobinaabasi/FallActivity.kt
@@ -1,11 +1,36 @@
 package com.example.mobinaabasi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobinaabasi.databinding.ActivityFallBinding
+
+
 
 class FallActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFallBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fall)
+        binding = ActivityFallBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.backBtn.setOnClickListener {
+
+            Intent(this,MainActivity::class.java).also {
+                startActivity(it)
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
     }
 }

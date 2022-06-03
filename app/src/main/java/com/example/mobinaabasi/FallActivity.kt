f3415623a3d1aa4f2cb93f3cc6b3c2d52be98258
Yadzan <104123429+YazdanMne@users.noreply.github.com>
2022-06-02 21:39:29 -0700

    University_project_fal_hafez

---
+++ app/src/main/java/com/example/mobinaabasi/FallActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobinaabasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fall)
+    }
+}
--- app/src/main/java/com/example/mobinaabasi/MainActivity.kt
+++ app/src/main/java/com/example/mobinaabasi/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.mobinaabasi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobinaabasi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnFal.setOnClickListener {
+
+            Intent()
+
+
+        }
+
+
+
+
+
     }
 }
